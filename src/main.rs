47d825ebe526d6ed097f3116ffe397a7791a8123
jdvr <juandavidvegarguez@gmail.com>
2018-05-28 00:46:03 +0200

    create a hash and calculate a path for the target message

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,26 @@ use std::env;
 use futures::Stream;
 use tokio_core::reactor::Core;
 use telegram_bot::*;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+
+struct MessageHasher {
+    message: Message
+}
+
+impl Hash for  MessageHasher {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.message.id.hash(state);
+        self.message.date.hash(state);
+    }
+}
+
+fn to_hash<T: Hash>(t: &T) -> u64 {
+    let mut s = DefaultHasher::new();
+    t.hash(&mut s);
+    s.finish()
+}
+
 
 fn main() {
     let mut core = Core::new().unwrap();
@@ -19,7 +39,6 @@ fn main() {
 
     // Fetch new updates via long poll method
     let future = api.stream().for_each(|update| {
-        println!("{:?}", update);
         // If the received update contains a new message...
         if let UpdateKind::Message(message) = update.kind {
             
@@ -30,7 +49,10 @@ fn main() {
                         let mut r = String::from("No target message found");
                         if let MessageOrChannelPost::Message(target_message) = *message_or_post {    
                             if let MessageKind::Text {ref data, ..} = target_message.kind {
-                                r = format!("I am going to index \n{}\n in '{}'", data, index);
+                                let message_id = to_hash(&MessageHasher { message: target_message.clone() });
+                                let path = format!("/data/message_index/{}/{}/{}.text", &target_message.from.first_name, index, message_id);
+
+                                r = format!("I am going to index \n{}\n in '{}'\n with id: {} in {}", data, index, message_id, path);
                             }
                         }
                         r
